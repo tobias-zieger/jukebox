6a8d7dd7cac3ffb548810cd9730d3437e27192d9
Tobias Zieger <tobias@vogel.name>
2022-04-02 23:37:17 +0200

    A lot of improvements:
    - included the cover in the player
    - added random cd select
    - put a lot of code into functions and untangled some code
    - ids are not derived from the cd name and will probably not change
    - highlighting the yet unplayed cds
    - added a heatmap functionality and a newness indicator
    - added a screenshot as file

---
+++ main.js
@@ -0,0 +1,122 @@
+function prepareData() {
+  // Iterate over the CDs and enrich them and fill the categories mapping with it.
+  groupedCds = {}
+
+  for (cd of cds) {
+    // Enrich the CD with its long title.
+    cd.longtitle = (cd.category ? cd.category + " - " : "") + cd.name
+    // Enrich the CDs with IDs. We need that to have fixed references (for #links and to recognize them regarding their usage). Numeric IDs are bad here, because they would be unstable when a CD is added or removed.
+    id = calculateId(cd);
+
+    // In case of an ID clash, complain about it, ignore it, and continue. The first CD wins.
+    if (Object.keys(cdDictionary).includes(id)) {
+      console.warn("The ID for CD \"" + cd.name + "\" (\"" + id + "\") was already generated for a different CD. The new CD will be ignored.");
+      continue;
+    }
+    cd.id = id;
+    cdDictionary[id] = cd;
+
+    // Sort the CD into a group for that category.
+    group = (groupedCds.hasOwnProperty(cd.category) ? groupedCds[cd.category] : [])
+    group.push(cd)
+    groupedCds[cd.category] = group
+  }
+  
+  // Store and sort the categories.  
+  categories = Array.from(Object.keys(groupedCds)).sort(isort);
+}
+
+function writePreloadFooterImages() {
+  // preload the images of the footer as early as possible
+  document.write("<div style=\"display:none\">");
+  for (category of categories) {
+    if (category != "") {
+      document.write("<img src=\"category%20pictures/" + category + ".jpg\">");
+    }
+  }
+  document.write("</div>");
+}
+
+function writeCDs() {
+  for (category of categories) {
+    if (category != "") {
+      document.write("<h1 id=\"" + category + "\">" + category + "</h1>")
+    }
+    document.write("<ul class=\"cdlist\">");
+    for (cd of groupedCds[category].sort(objectsort)) {
+      cdsInOrder.push(cd);
+
+      isNewCd = getNumberOfTimesPlayed(cd.id) < 1;
+
+      // Add it to the page.
+      document.write("<li class=\"CD\" id=\"" + cd.id + "\">")
+      document.write("  <div class=\"coverWithDuration\">")
+      document.write("    <img class=\"coverImage\" src=\"" + cd["cover"] + "\" title=\"" + cd["name"] + "\">")
+      document.write("    <span class=\"duration\">" + cd["duration"] + "</span>")
+      document.write("  </div>")
+      document.write("  <span class=\"name" + (isNewCd ? " new" : "") + "\">" + cd.name + "</span>")
+      document.write("</li>");
+    }
+    document.write("</ul>")
+  }
+}
+
+function enableEventsForCDs() {
+  for (cdElement of htmlCollectionToArray(document.getElementsByClassName("CD"))) {
+    cdElement.onclick = function(e) {
+      e.preventDefault();
+      elm = e.target;
+
+      // Bubble up to find LI from the current element.
+      while (true) {
+        if (elm.nodeName == "LI")
+          break;
+        else
+          elm = elm.parentElement;
+      }
+
+      cd = cdDictionary[elm.id];
+
+      playCd(cd);
+    };
+  }
+}
+
+function writeFooter() {
+  // Show the footer only if it actually contains something.
+  if (categories.length <= 1) {
+    return;
+  }
+  document.write("<div id=\"footer\">")
+  document.write("<span class=\"helper\"></span>")
+  for (category of categories) {
+    if (category != "") {
+      document.write("<a href=\"#" + category + "\"><img class=\"categoryPicture\" title=\"" + category + "\" src=\"category%20pictures/" + category + ".jpg\"></a>")
+    }
+  }
+  document.write("</div>");
+}
+
+// Clear the usage history upon request.
+if (location.search == "?clear") {
+  alert("Clearing Local Storage…");
+  localStorage.clear(); 
+}
+
+autoplay = false;
+
+// For each CD, make an object containing the cover image and the audio file for later access.
+cdDictionary = {};
+
+// For autoplay, we need a structure for the CDs that we can cycle through easily.
+cdsInOrder = []
+
+heatmapVisible = false;
+
+prepareData();
+writePreloadFooterImages();
+writeCDs();
+enableEventsForCDs();
+writeFooter();
+refreshHeatmapOverlayColors();
+
---
+++ playing.js
@@ -0,0 +1,124 @@
+function playCd(selectedCd) {
+  player = document.getElementById("player");
+  player.style.visibility = "visible";
+
+  playingtitle = document.getElementById("playingtitle");
+  
+  playingtitle.textContent = selectedCd["longtitle"];
+
+  playingcover = document.getElementById("playingcover");
+  playingcover.src = selectedCd["cover"];
+
+  playingcoverlink = document.getElementById("playingcoverlink");
+  playingcoverlink.href = "#" + selectedCd["id"];
+
+  audio = document.getElementById("audiocontrol");
+
+  // Clean its content so that we know that it's empty in the following.
+  audio.textContent = "";
+
+  source = document.createElement("source");
+  source.setAttribute("id", "audiosource");
+  source.setAttribute("src", selectedCd["audio"]);
+  audio.appendChild(source)
+
+  // Preload the audio without playing.
+  audio.load();
+
+  // Actually start playing.
+  audio.play();
+
+  refreshHeatmapOverlayColors();
+
+  // Remove all shadows.
+  for (coverImage of htmlCollectionToArray(document.getElementsByClassName("coverImage"))) {
+    coverImage.style.boxShadow = "";
+  }
+
+  // Add a shadow for the one that was chosen.
+  // For the autoplay to work, we cannot derive that from the element that was clicked on but from the CD that is playing, because there is no click when autoplaying.
+  // Fake a click.
+  elm = document.getElementById(selectedCd.id);
+  imageElement = elm.querySelector("div img");
+  imageElement.style.boxShadow = "0px 0px 10px 10px deepskyblue";
+  
+  // mark this cd as having been played (once again)
+  // remove the new mark
+  elm.querySelector("span.name").classList.remove("new");
+  
+  // save it to local storage
+  numberOfTimesPlayed = getNumberOfTimesPlayed(selectedCd.id);
+  localStorage.setItem(selectedCd.id, ++numberOfTimesPlayed);
+}
+
+function continueOnAutoplay() {
+  if (!autoplay) {
+    // no autoplay
+    return;
+  }
+  currentIndex = cdsInOrder.indexOf(cd);
+  nextIndex = currentIndex + 1
+  if (nextIndex == cdsInOrder.length) {
+    nextIndex = 0;
+  }
+  nextCd = cdsInOrder[nextIndex];
+
+  // This is the global variable that hold the currently played CD.
+  cd = nextCd;
+
+  playCd(nextCd);
+}
+
+function toggleAutoplay() {
+  autoplay = !autoplay;
+  newColor = autoplay ? "deepskyblue" : "black";
+  document.getElementById("autoplay-panel").style.color = newColor;
+}
+
+function playRandomCd() {
+  cdKeys = Object.keys(cdDictionary);
+  randomIndex = Math.floor(Math.random() * cdKeys.length);
+  randomCd = cdDictionary[cdKeys[randomIndex]];
+  playCd(randomCd);
+}
+
+function refreshHeatmapOverlayColors() {
+  // Recalculate and change the style of each overlay.
+  // The minimum is 0. Find the maximum value to interpolate the color. Start with a maximum of 1 to avoid a division by zero below.
+  maximum = 1;
+  
+  for (cd of cds) {
+    maximum = Math.max(maximum, getNumberOfTimesPlayed(cd.id));
+  }
+  
+  for (cd of cds) {
+    // calculate the color value
+    fraction = getNumberOfTimesPlayed(cd.id) / maximum;
+    // We go from yellow (60) to red (0).
+    colorValue = 60 - fraction * 60;
+    // We misuse this field because we have no other convenient way to store the hue.
+    document.getElementById(cd.id).style.animationIterationCount = colorValue;
+  }
+  
+  updateBackgroundColor();
+}
+
+function toggleHeatmap() {
+  heatmapVisible = !heatmapVisible;
+  newColor = heatmapVisible ? "deepskyblue" : "black";
+  document.getElementById("heatmap-panel").style.color = newColor;
+  
+  for (element of htmlCollectionToArray(document.getElementsByClassName("coverImage"))) {
+    element.style.opacity = (heatmapVisible ? "0.5" : "1");
+  }
+  
+  updateBackgroundColor();
+}
+
+function updateBackgroundColor() {
+  for (element of htmlCollectionToArray(document.getElementsByClassName("CD"))) {
+    colorValue = document.getElementById(element.id).style.animationIterationCount;
+    element.style.backgroundColor = "hsla(" + colorValue + ", 100%, 50%, " + (heatmapVisible ? "1" : "0") + ")";
+  }
+}
+
